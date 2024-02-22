9764a932b772aaa558ede223ed99ccee62e59b9f
harithepersistent <harikrishnan476@gmail.com>
2024-02-22 11:49:00 +0530

    Hall Booking app completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,12 @@ import roomRouter from './room.js'
 
 const router = expres.Router();
 router.get("/", (req, res) => {
-    res.status(200).send(` <h1>Welcome to  Hall Booking Web Application</h1>`);
+    res.status(200).send(` <head>
+    <style>
+        h1 {
+            color: blue;
+            font-family: Arial, sans-serif; }</style>
+            <h1>Welcome to Hall Booking Web Application</h1>`);
   });
 
 
